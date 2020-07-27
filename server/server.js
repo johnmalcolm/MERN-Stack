41b4d32b4357ae0a3d4f1c3b446d74bbad4ad099
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-07-27 10:47:48 +0100

    added graphql

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const app = express();
 
 const fileServerMiddleware = express.static('public');
 
+const typeDefs = `
+    type Query {
+        about String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!): String
+    }
+`;
+
 app.use('/public', fileServerMiddleware);
 
 app.listen(3000, function(){
