34d93544600515ae3dc76550cf7192e2dccd15f3
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-07-31 12:26:14 +0100

    displaying errors

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,33 @@ function IssueTable(props){
 
 const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 
+async function graphQLFetch(query, variables = {}){
+    try {
+        const response = await fetch('/graphql',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ query, variables })
+        });
+
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors){
+            const error = result.errors[0];
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else{
+                alert(`${error.extensions.code}: ${error.message}`)
+            }
+        }
+        return result.data;
+    } catch (e) {
+        alert(`Error in sending data to server: ${e.message}`)
+    }
+    
+}
+
 function jsonDateReviver(key, value) {
     if (dateRegex.test(value)) return new Date(value); return value;
 }
@@ -118,12 +145,10 @@ class IssueList extends React.Component{
             }
         }`;
 
-        const reponse = await fetch('/graphql',{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ query, variables: { issue } })
-        })
-        this.loadData();
+        const data = await graphQLFetch(query, { issue });
+        if (data){
+            this.loadData();
+        }
     }
 
     async loadData(){
@@ -131,12 +156,10 @@ class IssueList extends React.Component{
             id title status owner
             created effort due
           }}`;
-        const response = await fetch('/graphql', { method: 'POST',
-            headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({ query })
-        });
-        const body = await response.text();
-        const result = JSON.parse(body, jsonDateReviver);
-        this.setState({ issues: result.data.issueList });
+        const data = await graphQLFetch(query);
+        if (data){
+            this.setState({issues: data.issueList});
+        }
     }
 
     render(){
