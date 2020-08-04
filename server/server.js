e9068efde50887687245c653b191b7a2d1342fc1
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-08-04 18:20:22 +0100

    Read/Write with MongoDB

--- server/server.js
+++ server/server.js
@@ -9,8 +9,50 @@ require('dotenv').config();
 const password = process.env.MONGO_DB_PASS;
 
 const url = `mongodb+srv://john123:${password}@scientometrics-cluster.c3y2t.mongodb.net/development?retryWrites=true&w=majority`
-const client = new MongoClient(url);
-client.connect();
+
+function testWithCallbacks(callback){
+  console.log('\n --- Test with callbacks ---')
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  client.connect(function(err, client){
+    if( err ){
+      callback(err);
+      return;
+    }
+    console.log('Connected to MongoDB');
+    const db = client.db();
+    const collection = db.collection('employees');
+    
+    const employee = {
+      id: 1,
+      name: 'A. Callback',
+      age: 23
+    };
+    collection.insertOne(employee, function(err, result){
+      if( err ){
+        callback(err);
+        return;
+      }
+      console.log('result of insert:\n', result.insertedId)
+      collection.find({_id: result.insertedId})
+        .toArray(function(err, docs){
+          if( err ){
+            callback(err);
+            return;
+          }
+          console.log('result of find:\n', docs)
+          client.close();
+          callback(err);
+        })
+    })
+  });
+}
+
+testWithCallbacks(function(err){
+  if(err){
+    console.log(err);
+  }
+})
+
 
 // In Server Memory DB (Temp)
 
