ce87a57053a56254f003388adea23bfbc7665d11
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-08-05 13:03:39 +0100

    added async version

--- server/scripts/trymongo.js
+++ server/scripts/trymongo.js
@@ -9,6 +9,8 @@ const url = `mongodb+srv://john123:${password}@scientometrics-cluster.c3y2t.mong
 function testWithCallbacks(callback){
   console.log('\n --- Test with callbacks ---')
   const client = new MongoClient(url, { useNewUrlParser: true });
+
+  // Connect to DB
   client.connect(function(err, client){
     if( err ){
       callback(err);
@@ -23,12 +25,16 @@ function testWithCallbacks(callback){
       name: 'A. Callback',
       age: 23
     };
+
+    // Insert an object
     collection.insertOne(employee, function(err, result){
       if( err ){
         callback(err);
         return;
       }
       console.log('result of insert:\n', result.insertedId)
+
+      // Find an object
       collection.find({_id: result.insertedId})
         .toArray(function(err, docs){
           if( err ){
@@ -43,8 +49,42 @@ function testWithCallbacks(callback){
   });
 }
 
+// Async/Await paradigm is cleaner.
+async function testWithAsync(){
+  console.log('\n --- Test with Async/Await ---')
+  const client = new MongoClient(url, { useNewUrlParser: true });
+
+  try {
+    // Connect to DB
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('employees');
+      
+    const employee = {
+      id: 1,
+      name: 'A. Callback',
+      age: 23
+    };
+
+    // Insert an object
+    const result = await collection.insertOne(employee);
+    console.log('result of insert:\n', result.insertedId)
+
+    // Find an object
+    const docs = await collection.find({_id: result.insertedId}).toArray();
+    console.log('result of find:\n', docs);
+  } catch (err) {
+    console.log(err);
+  } finally{
+    client.close();
+  }
+
+}
+
+// Test Callback paradigm
 testWithCallbacks(function(err){
   if(err){
     console.log(err);
   }
+  testWithAsync();
 })
