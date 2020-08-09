11e361a7f1c3c98f1e94ed77cc3d83c6826977bd
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-08-09 19:06:29 +0100

    http proxy middleware

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 const port = process.env.UI_SERVER_PORT;
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT;
 const env = {UI_API_ENDPOINT};
+
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+if (apiProxyTarget){
+    app.use('/graphql', proxy({target: apiProxyTarget}))
+}
+
 
 app.use(express.static('public'));
 
