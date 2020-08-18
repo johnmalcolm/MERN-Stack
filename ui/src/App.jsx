9483eb42a8bd2b83d56b06fd63c96196e50b03b4
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-08-18 12:45:52 +0100

    ESLINT config for UI Server & React

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,178 +1,170 @@
-class IssueFilter extends React.Component{
-    render(){
-       return(
-        <p>This is a placeholder for an issue filter</p>
-       )
-    }
+/* eslint "react/react-in-jsx-scope": "off" */
+/* globals React ReactDOM */
+/* eslint "react/jsx-no-undef": "off" */
+/* eslint "react/no-multi-comp": "off" */
+/* eslint "no-alert": "off" */
+
+// eslint-disable-next-line react/prefer-stateless-function
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <p>This is a placeholder for an issue filter</p>
+    );
+  }
 }
 
-const initialssues = [
-    {
-        id: 1, status: 'New', owner: 'Ravan', effort: 5,
-        created: new Date('2018-08-15'), due: undefined,
-        title: 'Error in console when clicking Add',
-    },
-    {
-        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-        created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-        title: 'Missing bottom border on panel',
-    },
-];
+function IssueTable({ issues }) {
+  const issueRows = issues.map(issue => <IssueRow issue={issue} key={issue.id} />);
 
-function IssueTable(props){
-        const issueRows = props.issues.map(issue => <IssueRow issue={issue} key={issue.id} />)
-
-        return(
-            <table className="bordered-table">
-            <thead>
-                <tr style={{borderCollapse: "collapse"}}>
-                    <th>ID</th>
-                    <th>Status</th>
-                    <th>Owner</th>
-                    <th>Created</th>
-                    <th>Effort</th>
-                    <th>Due Date</th>
-                    <th>Title</th>
-                </tr>
-            </thead>
-            <tbody>
-                {issueRows}
-            </tbody>
-        </table>
-        )
+  return (
+    <table className="bordered-table">
+      <thead>
+        <tr style={{ borderCollapse: 'collapse' }}>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Owner</th>
+          <th>Created</th>
+          <th>Effort</th>
+          <th>Due Date</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        {issueRows}
+      </tbody>
+    </table>
+  );
 }
 
 const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
 
-async function graphQLFetch(query, variables = {}){
-    try {
-        const response = await fetch(window.ENV.UI_API_ENDPOINT,{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ query, variables })
-        });
-
-        const body = await response.text();
-        const result = JSON.parse(body, jsonDateReviver);
-
-        if (result.errors){
-            const error = result.errors[0];
-            if (error.extensions.code == 'BAD_USER_INPUT') {
-                const details = error.extensions.exception.errors.join('\n ');
-                alert(`${error.message}:\n ${details}`);
-            } else{
-                alert(`${error.extensions.code}: ${error.message}`)
-            }
-        }
-        return result.data;
-    } catch (e) {
-        alert(`Error in sending data to server: ${e.message}`)
-    }
-    
-}
-
 function jsonDateReviver(key, value) {
-    if (dateRegex.test(value)) return new Date(value); return value;
+  if (dateRegex.test(value)) return new Date(value); return value;
 }
 
-function IssueRow(props){
-    const issue = props.issue;
-        return(
-            <React.Fragment>
-                <tr>
-                    <td>{issue.id}</td>
-                    <td>{issue.status}</td>
-                    <td>{issue.owner}</td>
-                    <td>{issue.created.toDateString()}</td>
-                    <td>{issue.effort}</td>
-                    <td>{issue.due ? issue.due.toDateString() : ''}</td>
-                    <td>{issue.title}</td>
-                </tr>
-            </React.Fragment>
-        )
+function IssueRow({ issue }) {
+  return (
+    <React.Fragment>
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.due ? issue.due.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    </React.Fragment>
+  );
 }
 
-class IssueAdd extends React.Component{
+async function graphQLFetch(query, variables = {}) {
+  try {
+    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query, variables }),
+    });
 
-    constructor(){
-        super();
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const body = await response.text();
+    const result = JSON.parse(body, jsonDateReviver);
+
+    if (result.errors) {
+      const error = result.errors[0];
+      if (error.extensions.code === 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
+      } else {
+        alert(`${error.extensions.code}: ${error.message}`);
+      }
     }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        const form = document.forms.issueAdd; 
-
-        const issue = {
-            owner: form.owner.value, 
-            title: form.title.value, 
-            due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
-        }
-
-        this.props.createIssue(issue);
-        form.owner.value = ""; 
-        form.title.value = ""; 
-    }
-
-    render(){
-        return(
-            <form name="issueAdd" onSubmit={this.handleSubmit}>
-                <input type="text" name="owner" placeholder="Owner" /> 
-                <input type="text" name="title" placeholder="Title" /> 
-                <button>Add</button>
-            </form>
-        )
-    }
+    return result.data;
+  } catch (e) {
+    alert(`Error in sending data to server: ${e.message}`);
+    return null;
+  }
 }
 
-class IssueList extends React.Component{
+class IssueAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    constructor(){
-        super();
-        this.state = {issues: []}
-        this.createIssue = this.createIssue.bind(this);
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.issueAdd;
 
-    componentDidMount(){
-        this.loadData();
-    }
+    const issue = {
+      owner: form.owner.value,
+      title: form.title.value,
+      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
+    };
 
-    async createIssue(issue){
+    const { createIssue } = this.props;
+    createIssue(issue);
+    form.owner.value = '';
+    form.title.value = '';
+  }
 
-        const query = `mutation issueAdd($issue: IssueInputs!){
+  render() {
+    return (
+      <form name="issueAdd" onSubmit={this.handleSubmit}>
+        <input type="text" name="owner" placeholder="Owner" />
+        <input type="text" name="title" placeholder="Title" />
+        <button type="submit">Add</button>
+      </form>
+    );
+  }
+}
+
+class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!){
             issueAdd(issue: $issue){
                 id
             }
         }`;
 
-        const data = await graphQLFetch(query, { issue });
-        if (data){
-            this.loadData();
-        }
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
     }
+  }
 
-    async loadData(){
-        const query = `query { issueList {
+  async loadData() {
+    const query = `query { issueList {
             id title status owner
             created effort due
           }}`;
-        const data = await graphQLFetch(query);
-        if (data){
-            this.setState({issues: data.issueList});
-        }
+    const data = await graphQLFetch(query);
+    if (data) {
+      this.setState({ issues: data.issueList });
     }
+  }
 
-    render(){
-        return(
-            <React.Fragment>
-                <h1>Issue Tracker</h1>
-                <IssueFilter></IssueFilter>
-                <IssueTable issues={this.state.issues}></IssueTable>
-                <IssueAdd createIssue={this.createIssue}/>
-            </React.Fragment>
-        )
-    }
+  render() {
+    const { issues } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <IssueTable issues={issues} />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+    );
+  }
 }
 
-const element = <IssueList />
-ReactDOM.render(element, document.getElementById('contents'))
+const element = <IssueList />;
+ReactDOM.render(element, document.getElementById('contents'));
--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,24 +1,25 @@
 const express = require('express');
+
 const app = express();
 const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 const port = process.env.UI_SERVER_PORT;
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT;
-const env = {UI_API_ENDPOINT};
+const { UI_API_ENDPOINT } = process.env;
+const env = { UI_API_ENDPOINT };
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
-if (apiProxyTarget){
-    app.use('/graphql', proxy({target: apiProxyTarget}))
+if (apiProxyTarget) {
+  app.use('/graphql', proxy({ target: apiProxyTarget }));
 }
 
 
 app.use(express.static('public'));
 
-app.get('/env.js', function(req, res){
-    res.send(`window.ENV = ${JSON.stringify(env)}`)
-})
+app.get('/env.js', (req, res) => {
+  res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
 
-app.listen(port, function(){
-    console.log(`UI server started on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`UI server started on port ${port}`);
+});
