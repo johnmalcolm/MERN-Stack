c136d4019170247eb2a95f2bfd3d1d86e146217f
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-08-26 10:06:51 +0100

    Enabled Hot Module Replacement

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -8,6 +8,26 @@ const port = process.env.UI_SERVER_PORT;
 const { UI_API_ENDPOINT } = process.env;
 const env = { UI_API_ENDPOINT };
 
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+
+if (enableHMR && (process.env.NODE_ENV !== 'production')){
+  console.log('Adding dev middleware, enabling HMR');
+  /* eslint "global-require": "off" */
+  /* eslint "import/no-extraneous-dependencies": "off" */
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  const config = require('./webpack.config.js');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
   app.use('/graphql', proxy({ target: apiProxyTarget }));
